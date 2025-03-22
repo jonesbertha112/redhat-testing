ea26ba85b10f8a1ddbc43ac80626f3b3a59c5df7
jonesbertha112 <jonesbertha112@gmail.com>
2025-03-22 09:24:14 +0700

    Update droangon.py

--- droangon.py
+++ droangon.py
@@ -1,1 +1,1 @@
-If code comments are longer than the code, you’re doing it wrong.The only constant in software development is change.Hello, world!droangon.py
+Exception in thread "main" java.lang.NullPointerException.If code comments are longer than the code, you’re doing it wrong.The only constant in software development is change.Hello, world!droangon.py
