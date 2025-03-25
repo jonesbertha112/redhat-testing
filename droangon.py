30793295757c034675475a0fb7d9196682b8c22c
jonesbertha112 <jonesbertha112@gmail.com>
2025-03-25 09:24:38 +0700

    Update droangon.py

--- droangon.py
+++ droangon.py
@@ -1,1 +1,1 @@
-Code like there is no tomorrow.Exception in thread "main" java.lang.NullPointerException.If code comments are longer than the code, you’re doing it wrong.The only constant in software development is change.Hello, world!droangon.py
+Software engineering is just applied laziness.Code like there is no tomorrow.Exception in thread "main" java.lang.NullPointerException.If code comments are longer than the code, you’re doing it wrong.The only constant in software development is change.Hello, world!droangon.py
